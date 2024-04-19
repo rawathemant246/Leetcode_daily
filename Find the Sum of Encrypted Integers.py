9b1f30dccc8e5e5139a1f07fe384717295e3a47e
rawathemant246 <hemantrawat246@gmail.com>
2024-04-19 12:29:03 +0530

    sum of encrypted integers

---
+++ Find the Sum of Encrypted Integers.py
@@ -0,0 +1,39 @@
+from typing import List
+
+class Solution(object):
+    
+    def sumofEncryptedNumber(self, nums: List[int]) -> int:
+        
+        def encrypt(x):
+            
+            s = str(x)
+            
+            max_digit = max(s)
+            
+            len_digit = len(s)
+            
+            
+            encypted_number = int(max_digit * len_digit)
+            
+            return encypted_number
+        
+        total_sum =0
+        
+        for number in nums:
+            
+            total_sum += encrypt(number)
+            
+        
+        
+        return total_sum
+    
+
+nums1 = [1,2,3]
+
+nums2 = [21,23,42]
+
+test1 = Solution()
+
+result = test1.sumofEncryptedNumber(nums2)
+
+print(result)
