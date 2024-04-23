59353fa5587a15aa85ecdb5228e2d6f97bfc7d33
rawathemant246 <hemantrawat246@gmail.com>
2024-04-23 10:49:13 +0530

    minimum-operation-to-exceed-threshold

---
+++ Minimum Operations to Exceed Threshold Value I.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution(object):
+    
+    def minOperation(self, nums: List[int], k:int ) -> int:
+        
+        nums.sort()
+        
+        
+        for num in nums:
+            
+            if num<k:
+                
+                count +=1
+            
+            else:
+                break
+        
+        
+        return count
+    
+    
