9d5576e6847e6f3fc45d4dcfc67b54a754ee69e4
rawathemant246 <hemantrawat246@gmail.com>
2024-04-29 13:53:53 +0530

    Count Prefix and suffix pairs

---
+++ Count Prefix and Suffix Pairs I.py
@@ -0,0 +1,34 @@
+from typing import List
+
+
+class Solution(object):
+    
+    
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        
+        
+        def isGood(i,j):
+            
+            return words[j].startswith(words[i]) & words[j].endswith(words[i])
+        
+    
+    
+        N = len(words)
+        
+        
+        count = 0
+        
+        
+        
+        for i in range(N):
+            
+            for j in range(i+1 , N):
+                
+                if isGood(i,j):
+                    
+                    count +=1
+                    
+        return count
+    
+    
+    
