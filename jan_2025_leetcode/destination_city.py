1586fcfb3d88cf2a57c6fed4d1c050c02c271d80
rawathemant246 <hemantrawat246@gmail.com>
2025-01-07 13:05:25 +0530

    destination city

---
+++ jan_2025_leetcode/destination_city.py
@@ -0,0 +1,16 @@
+
+from typing import List
+class Solution:
+    def destCity(self, paths: List[List[str]]) -> str:
+        start = set()
+        end = set()
+        for path in paths:
+            start.add(path[0])
+            end.add(path[1])
+        
+        return (end - start).pop()
+
+if __name__ == "__main__":
+    paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
+    sol = Solution()
+    print(sol.destCity(paths))
