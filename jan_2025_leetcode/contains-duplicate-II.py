86297aac617c211abc91041350a3c089bcf83b5d
rawathemant246 <hemantrawat246@gmail.com>
2025-01-27 09:18:55 +0530

    added problem statement

--- jan_2025_leetcode/contains-duplicate-II.py
+++ jan_2025_leetcode/contains-duplicate-II.py
@@ -1,3 +1,25 @@
+'''
+Contains Duplicate II
+
+nums : List[int]
+k : int 
+return : bool 
+
+True :  agar different indices pr same element ho means nums[i] == nums[j]
+and abs(i-j)<=k
+
+example 1
+
+nums = [1,2,3,1] k =3
+
+nums[0] == nums[3]  both are 1  and second condition  abs(0-3) <= 3
+
+example 2 :
+
+nums = [1,2,3,1,2,3] k =2
+
+'''
+
 from typing import List
 
 
