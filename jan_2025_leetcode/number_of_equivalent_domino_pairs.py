0462c274210524477a128714bee0417dd12c833c
rawathemant246 <hemantrawat246@gmail.com>
2025-01-30 20:35:21 +0530

    Added main module to run

--- jan_2025_leetcode/number_of_equivalent_domino_pairs.py
+++ jan_2025_leetcode/number_of_equivalent_domino_pairs.py
@@ -9,3 +9,22 @@ class Solution:
         counter = Counter(dominoes)
         
         return sum(value*(value-1)//2 for value in counter.values())
+
+if __name__ == "__main__":
+    
+    solution = Solution()
+    
+    print(solution.numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6]]))
+    
+    print(solution.numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6],[1,2],[2,1]]))
+    
+    print(solution.numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6],[1,2],[2,1],[1,2],[2,1]]))
+    
+    print(solution.numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6],[1,2],[2,1],[1,2],[2,1],[1,2],[2,1]]))
+    
+    print(solution.numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6],[1,2],[2,1],[1,2],[2,1],[1,2],[2,1],[1,2],[2,1]]))
+    
+    print(solution.numEquivDominoPairs([[1,2],[2,1],[3,4],[5,6]]))
+
+
+    
