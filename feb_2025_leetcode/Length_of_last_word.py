ec74e44ee1e4d8b8742eb2363a729da2d1b529ef
rawathemant246 <hemantrawat246@gmail.com>
2025-02-11 21:21:18 +0530

    Solved Length of Last Word

--- feb_2025_leetcode/Length_of_last_word.py
+++ feb_2025_leetcode/Length_of_last_word.py
@@ -25,4 +25,33 @@ Explanation: The last word is "joyboy" with length 6.
 
 How to solve this problem:
 
+1. remove the trailing spaces from the string  (from starting and ending)
+2. initialize a counter to to counter the number of character 
+and if there is a space then reset the counter 
+
 '''
+
+class Solution:
+    def lengthofLastWord(self, s:str) -> int:
+
+        s = s.rstrip()
+
+        if not s:
+            return 0
+        
+        count = 0
+
+        for char in reversed(s):
+            if char == " ":
+                count +=1
+            
+            else:
+                break
+        return count
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.lengthofLastWord("Hello World"))
+    print(s.lengthofLastWord("   fly me   to   the moon  "))
+    print(s.lengthofLastWord("luffy is still joyboy"))
