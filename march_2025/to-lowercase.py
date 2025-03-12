88265ef30251c278a7f23f9922cc1aeb5ad7e337
rawathemant246 <hemantrawat246@gmail.com>
2025-03-12 18:22:09 +0530

    Solved Leetcode Easy to lowercase

---
+++ march_2025/to-lowercase.py
@@ -0,0 +1,28 @@
+'''
+Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+ 
+
+Example 1:
+
+Input: s = "Hello"
+Output: "hello"
+Example 2:
+
+Input: s = "here"
+Output: "here"
+Example 3:
+
+Input: s = "LOVELY"
+Output: "lovely"
+
+
+'''
+
+class Solution:
+    def tolowercase(self, s:str) -> str:
+        return s.lower()
+
+solution = Solution()
+result = solution.tolowercase("Hello")
+print(result)
