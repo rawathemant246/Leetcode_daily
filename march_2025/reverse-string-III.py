0557f80e24103b310cb8eff7219fb6de5edb09d4
rawathemant246 <hemantrawat246@gmail.com>
2025-03-13 22:03:12 +0530

    Solved reverse string III

---
+++ march_2025/reverse-string-III.py
@@ -0,0 +1,24 @@
+'''
+Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+
+ 
+
+Example 1:
+
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+Example 2:
+
+Input: s = "Mr Ding"
+Output: "rM gniD"
+
+
+'''
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join([i[::-1] for i in s.split()])
+
+if __name__ == "__main__":
+    sol = Solution()
+    
