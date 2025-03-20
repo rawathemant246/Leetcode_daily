d3ae64ea8027ccd8beee276c38429d989690c88e
rawathemant246 <hemantrawat246@gmail.com>
2025-03-20 07:55:05 +0530

    Longest substring without duplicate characters

---
+++ march_2025/Longest-substring-without-Repeating-characters.py
@@ -0,0 +1,28 @@
+'''
+Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+Example 1:
+
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+Example 2:
+
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+Example 3:
+
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+use the two pointer approach to solve this problem. The two pointer approach is a really efficient technique which is especially useful for strings and arrays. The technique uses two pointers to traverse the string or array and is really useful when you need to find a contiguous subarray or substring.
+
+
+
+'''
