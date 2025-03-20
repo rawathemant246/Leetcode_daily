bec754dfb9ffcb554459f8ab9c32d0aaed689472
rawathemant246 <hemantrawat246@gmail.com>
2025-03-20 08:08:29 +0530

    Solved longest substring

--- march_2025/Longest-substring-without-Repeating-characters.py
+++ march_2025/Longest-substring-without-Repeating-characters.py
@@ -1,9 +1,7 @@
 '''
 Given a string s, find the length of the longest substring without duplicate characters.
 
- 
-
-Example 1:
+ Example 1:
 
 Input: s = "abcabcbb"
 Output: 3
@@ -26,3 +24,82 @@ use the two pointer approach to solve this problem. The two pointer approach is 
 
 
 '''
+
+class Solution:
+    def lengthofSubstring(self, s: str) -> int:
+        char_set = set()
+        left = 0
+        max_length = 0 
+        
+        for right in range(len(s)):
+            while s[right] in char_set:
+                char_set.remove(s[left])
+                left +=1
+            char_set.add(s[right])
+            max_length = max(max_length, right - left + 1)
+        
+        return max_length
+# Time complexity: O(n)
+# Space complexity: O(min(n, m)) where m is the size of the character set
+# The space complexity is O(min(n, m)) because the size of the character set is fixed and will not exceed 26 characters.
+
+if __name__ == "__main__":
+    s = "abcabcbb"
+    print(Solution().lengthofSubstring(s))
+    s = "bbbbb"
+    print(Solution().lengthofSubstring(s))
+    s = "pwwkew"
+    print(Solution().lengthofSubstring(s))
+    s = " "
+    print(Solution().lengthofSubstring(s))
+    s = "au"
+    print(Solution().lengthofSubstring(s))
+    s = "aab"
+    print(Solution().lengthofSubstring(s))
+    s = "dvdf"
+    print(Solution().lengthofSubstring(s))
+    s = "tmmzuxt"
+    print(Solution().lengthofSubstring(s))
+    s = "abba"
+    print(Solution().lengthofSubstring(s))
+    s = "abcabcbb"
+    print(Solution().lengthofSubstring(s))
+    s = "bbbbb"
+    print(Solution().lengthofSubstring(s))
+    s = "pwwkew"
+    print(Solution().lengthofSubstring(s))
+    s = " "
+    print(Solution().lengthofSubstring(s))
+    s = "au"
+    print(Solution().lengthofSubstring(s))
+    s = "aab"
+    print(Solution().lengthofSubstring(s))
+    s = "dvdf"
+    print(Solution().lengthofSubstring(s))
+    s = "tmmzuxt"
+    print(Solution().lengthofSubstring(s))
+    s = "abba"
+    print(Solution().lengthofSubstring(s))
+    s = "abcabcbb"
+    print(Solution().lengthofSubstring(s))
+    s = "bbbbb"
+    print(Solution().lengthofSubstring(s))
+    s = "pwwkew"
+    print(Solution().lengthofSubstring(s))
+    s = " "
+    print(Solution().lengthofSubstring(s))
+    s = "au"
+    print(Solution().lengthofSubstring(s))
+    s = "aab"
+    print(Solution().lengthofSubstring(s))
+    s = "dvdf"
+    print(Solution().lengthofSubstring(s))
+    s = "tmmzuxt"
+    print(Solution().lengthofSubstring(s))
+    s = "abba"
+    print(Solution().lengthofSubstring(s))
+    s = "abcabcbb"
+    print(Solution().lengthofSubstring(s))
+    s = "bbbbb"
+    print(Solution().lengthofSubstring(s))
+    
